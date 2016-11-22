fb5ecddbf741e5931be526544904ae5150234e4e
Calin <calinoiu.alexandru@agilefreaks.com>
2016-11-22 10:57:36 +0530

    Add parameters

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,19 +2,14 @@
 FIZZ = 'Fizz'
 BUZZ = 'Buzz'
 
-def divisible_by?(numerator, denominator)
-  (numerator % denominator).zero?
-end
-
 def fizzbuzz(range, triggers)
   range.each do |i|
     result = []
     triggers.each do |divisor, text|
-      result << text if divisible_by?(i, divisor)
+      result << text if (i % divisor).zero?
     end
     p result.empty? ? i : result.join
   end
 end
 
 fizzbuzz(1..100, [[3, 'Fizz'], [5, 'Buzz']])
-
